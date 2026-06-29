85c968656dd16cb670c85f077966ca60819ee7b2
Daddarios <onurgoekhanbicer@gmail.com>
2026-06-29 20:22:01 +0300

    mail2

--- app.js
+++ app.js
@@ -1004,7 +1004,7 @@ const contentMap = {
               </form>
 
               <p class="mail-fallback">
-              oder direkt: <span id="kontaktMail" style="color: inherit;"></span>
+              oder direkt: <span id="kontaktMail" ></span>
               </p>
            </div>
         `
